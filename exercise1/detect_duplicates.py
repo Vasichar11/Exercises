edc9865887144f556017ce6e9e36b0dd44bd799d
Vasichar11 <vasichar11@hotmail.com>
2023-07-31 02:52:03 +0300

    Function passes first tests

--- exercise1/detect_duplicates.py
+++ exercise1/detect_duplicates.py
@@ -1,2 +1,10 @@
-def detect_duplicates():
-    pass
+def detect_duplicates(input_list=None):
+    # Time complexity of O(n^3), where n is the number of elements in the list
+    duplicates = []
+    for item in input_list:
+        # .count() will internally scan the list, giving time complexity O(n) on its own
+        # "in" operator has O(n) complexity for lists
+        if input_list.count(item) > 1 and item not in duplicates:
+            duplicates.append(item)
+
+    return duplicates
